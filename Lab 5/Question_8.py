2097f1a1f99cec316084de623cfcc62845d80693
Aman Upadhyay <amanupadhyay0208@gmail.com>
2022-03-27 00:06:24 +0530

    All Questions done

--- Lab 5/Question_8.py
+++ Lab 5/Question_8.py
@@ -1,9 +1,19 @@
 #! /bin/python3
 # Write a function in Python which accepts a string and returns the updated string where the uppercase alphabet will be replaced with lowercase and vice versa. also replace all the digits with # and all other charecters with %
 
-def replace_char(s):
-    
+def replacement(string):
+    Result = ""
+    for i in range(len(string)):
+        if string[i].isupper():
+            Result += string[i].lower()
+        elif string[i].islower():
+            Result += string[i].upper()
+        elif string[i].isdigit():
+            Result += '#'
+        else:
+            Result += '%'
+    return Result
 
 # Driver code
-s = input("Enter a string: ")
-print("The updated string is: ", replace_char(s))
+string  = input("Enter a string: ")
+print(replacement(string))
